8baf8108e12dafdc5255aeb6b981feaa1dd1dbb7
LEONE FABIO s330500 <s330500@studenti.polito.it>
2025-08-14 14:52:40 +0200

    Remove assertion for MODEL_PATH suffix in set_args function

--- modules/utils.py
+++ modules/utils.py
@@ -118,7 +118,6 @@ def set_args(args):
 
     args.MODEL_PATH = args.MODEL_PATH[:-1] if args.MODEL_PATH.endswith('/') else args.MODEL_PATH 
 
-    assert args.MODEL_PATH.endswith('kinetics-pt') or args.MODEL_PATH.endswith('imagenet-pt') 
     args.model_init = 'imagenet' if args.MODEL_PATH.endswith('imagenet-pt') else 'kinetics'
     
     if args.MODEL_PATH == 'imagenet':
