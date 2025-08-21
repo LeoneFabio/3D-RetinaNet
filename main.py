1bc5bd781b8206d6b8732f88a88faef7fa249ff2
LEONE FABIO s330500 <s330500@studenti.polito.it>
2025-08-21 15:01:05 +0200

    Add logging for concept extraction and dataset processing steps

--- main.py
+++ main.py
@@ -276,8 +276,8 @@ def main():
         val(args, net, val_dataset)
     elif args.MODE == 'gen_dets':
         gen_dets(args, net, val_dataset)
-        #eval_framewise_dets(args, val_dataset)
-        #build_eval_tubes(args, val_dataset)
+        eval_framewise_dets(args, val_dataset)
+        build_eval_tubes(args, val_dataset)
     elif args.MODE == 'eval_frames':
         eval_framewise_dets(args, val_dataset)
     elif args.MODE == 'eval_tubes':
